0cb4277b19de09013cf05d71b9e5085fa7b9da2a
PrimEverDream <62145972+PrimEverDream@users.noreply.github.com>
2020-04-24 20:42:46 +0200

    Ajout partie luminosité

--- app/src/main/java/com/example/robotbluetooth/Parametres.java
+++ app/src/main/java/com/example/robotbluetooth/Parametres.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,7 +27,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class Parametres extends AppCompatActivity {
-    SeekBar seekBar;
     TextView textView3, textView4, textView5, textBlue;
     ListView scanList;
     ArrayList<String> stringArrayList = new ArrayList<String>();
@@ -33,6 +36,9 @@ public class Parametres extends AppCompatActivity {
 
     Intent btEnablingIntent;
     int requestCodeForEnable;
+    //=================
+    SeekBar sbLumi;
+    boolean success = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +49,37 @@ public class Parametres extends AppCompatActivity {
         textView4 = findViewById(R.id.textView4);
         textView5 = findViewById(R.id.textView5);
 
+
         //gestion de la luminosité
-        seekBar = findViewById(R.id.seekBar);
+        sbLumi = findViewById(R.id.seekBar);
+        sbLumi.setMax(255);
+        sbLumi.setProgress(getLumi());
+
+        sbLumi.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser && success){
+                    setLumi(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (!success){
+                    Toast.makeText(Parametres.this,"Permission not granted",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
         //activation du blutooth
         buttonScan = findViewById(R.id.buttonScan);
 
@@ -75,11 +110,22 @@ public class Parametres extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == requestCodeForEnable) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Bluetooth disponible", Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Bluetooth arrêté", Toast.LENGTH_LONG).show();
+            }
+        }
+        if (requestCode == 1000) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                boolean value = Settings.System.canWrite(getApplicationContext());
+                if (value) {
+                    success = true;
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     }
@@ -102,4 +148,56 @@ public class Parametres extends AppCompatActivity {
         });
     }
 
+
+    //=========================================================================
+    //Fonctions pour la luminosite
+    private void setLumi (int lumi){
+        if (lumi < 0){
+            lumi = 0;
+        }
+        else if(lumi > 255){
+            lumi = 255;
+        }
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+        Settings.System.putInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS, lumi);
+    }
+
+    private int getLumi(){
+        int lumi = 100;
+        try {
+            ContentResolver contentResolver = getApplicationContext().getContentResolver();
+            lumi = Settings.System.getInt(contentResolver, Settings.System.SCREEN_BRIGHTNESS);
+        }catch(Settings.SettingNotFoundException e){
+            e.printStackTrace();
+        }
+        return lumi;
+    }
+
+    private void getPermission(){
+        boolean value;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            value = Settings.System.canWrite(getApplicationContext());
+            if(value){
+                success = true;
+            }else{
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                startActivity(intent);
+            }
+        }
+    }
+    //Version partielle
+    /*@Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1000) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                boolean value = Settings.System.canWrite(getApplicationContext());
+                if (value) {
+                    success = true;
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }*/
 }
